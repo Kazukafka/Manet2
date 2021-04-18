f1c2a79462c811c3551fdd3493b69d2c6632f358
Kazuhisa Noguchi <kazuhisanoguchi@KazuhisanoMacBook-Air.local>
2021-04-18 08:05:18 +0300

    MessagesDesign with RANGE

--- components/Messages.vue
+++ components/Messages.vue
@@ -1,9 +1,9 @@
 <template>
 <div>
-  <div class="chats-container">
+  <div class="box17">
     <message v-for="message in messages" :message="message"/>
+    <h1></h1>
   </div>
-  <h1></h1>
 </div>
 </template>
 
@@ -66,6 +66,42 @@ export default {
 h1 {
   border-bottom: solid 3px skyblue;
   position: relative;
+}
+
+.box7{
+    padding: 0.5em 1em;
+    margin: 2em 0;
+    color: #474747;
+    background: whitesmoke;/*背景色*/
+    border-left: double 7px #4ec4d3;/*左線*/
+    border-right: double 7px #4ec4d3;/*右線*/
+}
+.box7 p {
+    margin: 0; 
+    padding: 0;
+}
+
+.box17{
+    margin:2em 0;
+    position: relative;
+    padding: 0.5em 1.5em;
+    border-top: solid 2px black;
+    border-bottom: solid 2px black;
+}
+.box17:before, .box17:after{
+    content: '';
+    position: absolute;
+    top: -10px;
+    width: 2px;
+    height: -webkit-calc(100% + 20px);
+    height: calc(100% + 20px);
+    background-color: black;
+}
+.box17:before {left: 10px;}
+.box17:after {right: 10px;}
+.box17 p {
+    margin: 0; 
+    padding: 0;
 }
 
 h1:after {
